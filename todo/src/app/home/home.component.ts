7de03bd1ad82e46025d5ece3118de674ce43481d
Christopher Zuniga <c.zuniga21@gmail.com>
2020-09-28 21:32:29 -0600

    Updating home component.

--- todo/src/app/home/home.component.ts
+++ todo/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLoadList(){
+    this.router.navigate(['/list'])
   }
 
 }
