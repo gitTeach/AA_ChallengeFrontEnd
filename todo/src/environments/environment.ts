adda151f5198f1913e4c0fc6120c02144374436f
Christopher Zuniga <c.zuniga21@gmail.com>
2020-10-07 15:03:06 -0600

    Changing firebase project from aachallenge to netcoreauth
    This change was neccesary due an error in the backend when the middleware tried to validate token in the audience

--- todo/src/environments/environment.ts
+++ todo/src/environments/environment.ts
@@ -5,15 +5,24 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCxUsCAkJqsJA_3vlIVX6sJyY-GbtDOrWw",
-    authDomain: "aachallenge-c529d.firebaseapp.com",
-    databaseURL: "https://aachallenge-c529d.firebaseio.com",
-    projectId: "aachallenge-c529d",
-    storageBucket: "aachallenge-c529d.appspot.com",
-    messagingSenderId: "478287361648",
-    appId: "1:478287361648:web:5ec1b69c068393d6a766b5",
-    measurementId: "G-TGH1KWDCG2"
-  }
+
+    apiKey: "AIzaSyAZxjiVcitQ4mKLShKmVTg5O9GkLgd27vg",
+    authDomain: "netcoreauth.firebaseapp.com",
+    databaseURL: "https://netcoreauth.firebaseio.com",
+    projectId: "netcoreauth",
+    storageBucket: "netcoreauth.appspot.com",
+    messagingSenderId: "46871109885",
+    appId: "1:46871109885:web:3b58cf6a9cb2d865329e94"
+    // apiKey: "AIzaSyCxUsCAkJqsJA_3vlIVX6sJyY-GbtDOrWw",
+    // authDomain: "aachallenge-c529d.firebaseapp.com",
+    // databaseURL: "https://aachallenge-c529d.firebaseio.com",
+    // projectId: "aachallenge-c529d",
+    // storageBucket: "aachallenge-c529d.appspot.com",
+    // messagingSenderId: "478287361648",
+    // appId: "1:478287361648:web:5ec1b69c068393d6a766b5",
+    // measurementId: "G-TGH1KWDCG2"
+  },
+  apiUrl: 'http://localhost:5000/api'
 };
 
 /*
