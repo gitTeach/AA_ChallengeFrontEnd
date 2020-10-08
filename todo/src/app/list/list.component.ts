f217c19265fccd0ad730a2721df35b032d25ef75
Christopher Zuniga <c.zuniga21@gmail.com>
2020-10-07 21:43:00 -0600

    Preparing form to save a new List

--- todo/src/app/list/list.component.ts
+++ todo/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { auth } from 'firebase';
 import { first } from 'rxjs/operators';
 import { AuthService } from '../auth/services/auth.service';
@@ -12,15 +13,26 @@ import { List } from '../models/list';
 })
 export class ListComponent implements OnInit {
 
-  public list : any;
+  public listCollection : any;
+  public list : List;
   public user : any;
+  listForm: FormGroup;
+  submitted = false;
+  add = false;
 
   constructor(private listService : ListService,
-              private authService : AuthService) {
+              private authService : AuthService,
+              private formBuilder: FormBuilder) {
 
    }
 
   async ngOnInit() {
+
+    this.listForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      description: ['', [Validators.required, Validators.maxLength(200)]],
+    });
+
     this.user = await this.authService.getCurrentFirebaseUser();
     if (this.user){
       console.log('list component->', this.user);
@@ -28,12 +40,44 @@ export class ListComponent implements OnInit {
     }
   }
 
+  get currentListForm() {
+    return this.listForm.controls;
+  }
+
   getListsForUser(uid:string) {
-    this.list = this.listService.getListsForUser(uid);
+    this.listCollection = this.listService.getListsForUser(uid);
   }
 
   onClick(){
     console.log('click');
+  }
+  
+  createList(){
+
+    console.log(this.listForm.controls);
+    
+    this.submitted = true;
+
+    if (this.listForm.invalid) {
+      return;
+    }
+
+    const { name, description } = this.listForm.value;
+    this.list.name = name;
+    this.list.description = description;
+    this.list.userId = this.user.uid;
+    
+    this.listService.createListForUser(this.list, this.list.userId);
+    
+  }
+
+  addList(){
+    this.add = true;
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.listForm.reset();
   }
 
 }
